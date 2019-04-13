f569070b479e5b51fa49dc2f0b37d38fc7968c33
Sandra <grahlsan@gmail.com>
2019-04-13 19:22:05 +0100

    added the rest of the pages

--- src/pages/solution.js
+++ src/pages/solution.js
@@ -4,11 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Card from '../components/card'
+import buttonStyle from "../components/button.css"
 
 const Solution = () => (
   <Layout>
     <SEO title="Page two" />
     <p>Alternative Plastics</p>
+    <button style={buttonStyle}><Link to="/replace/">I want to stop using plastic!</Link></button> <button style={buttonStyle}><Link to="/recycle/">I want to figure out how to recycle plastic!</Link></button> <button style={buttonStyle}><Link to="/dispose/">I want to dispose of the plastic!</Link></button>
     <div style={{display:`flex`, justifyContent: `space-between`, flexWrap: `wrap` }}>
             <Card title='hello'/>
             <Card title='hello world'/>
