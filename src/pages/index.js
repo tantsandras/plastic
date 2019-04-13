481fdb32127aa12e08ef8d47b710a279245f1fa0
jihyun <jihyun@gosweat.com>
2019-04-13 15:28:27 +0100

    Accidently delelted and put it back

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,8 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
+    <HaveSolutionButton />
+    <NeedSolutionButton />
     <Link to="/solution/">Go to page 2</Link>
   </Layout>
 )
