5dbeac3c157ba43c17ed7848c2d54081402132ab
Helen Zhou <helen.zhou6@gmail.com>
2019-04-14 10:15:54 +0100

    Fix bugs and add form link up to local storage

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,10 +8,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import Header from "./Header"
 
-import Header from "./header"
 import "./layout.css"
-import Card from './card';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -36,7 +35,7 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-        
+
         </div>
       </>
     )}
---
+++ src/components/solutions.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react"
+
+import Card from './card'
+
+const Solutions = ({ type = 'replace' }) => {
+  const [solutions, setSolution] = useState([]);
+
+  useEffect(() => {
+    const storedSolutions = window.localStorage.getItem(type)
+    if (!storedSolutions) return
+    setSolution(JSON.parse(storedSolutions))
+  }, [setSolution, window.localStorage])
+
+  return (
+    <div style={{ display: `flex`, justifyContent: `space-between`, flexWrap: `wrap` }}>
+      {solutions.map(solution => <Card key={solution.name} title={solution.name} />)}
+    </div>
+  )
+}
+
+export default Solutions
